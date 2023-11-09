df1d89cdb3669f9270998c62e7f05588650ca2e1
Khoa hoc du lieu 17A1 <tvsy.dhkl17a1hn@sv.uneti.edu.vn>
2023-11-09 23:20:11 +0700

    sy

---
+++ bài tập 8.1.py
@@ -0,0 +1,8 @@
+a = eval(input("nhập giá trị a:"))
+b = eval(input("nhập giá trị b:"))
+c = eval(input("nhập giá trị c:"))
+d = eval(input("nhập giá trị d:"))
+max_value = max(a, b, c, d)
+min_value = min(a, b, c, d)
+print("đây là số lớn nhất:", max_value)
+print("đây là số nhỏ nhất:", min_value)
---
+++ bài tập 8.2.py
@@ -0,0 +1,4 @@
+x = int(input("nhập giá trị của x:"))
+abs_x = abs(x)
+print("giá trị tuyệt đối của x là:",abs_x)
+
